c5fd08883d3815985627aae83529445a1564835c
GeorgiaH <32138103+Georgia98@users.noreply.github.com>
2021-09-16 18:14:22 +0100

    Cleanup
    Removed unneccessary code

--- main.js
+++ main.js
@@ -1,20 +1,5 @@
 /*MAINJSFILE*/
 jQuery(document).ready(function ($) {
-    let root = document.documentElement;
-
-root.addEventListener("mousemove", e => {
-  root.style.setProperty('--mouse-x', e.clientX + "px");
-  root.style.setProperty('--mouse-y', e.clientY + "px");
-});
-
-
-        $("#primary-colour").change(function () {
-                root.style.setProperty('--primary-colour', this.value);
-        });
-    
-    $("#dark-night-toggle").change(function () {
-                $('body').toggleClass('lightmode');
-        });
     $('.work-image').slick({
   infinite: true,
   slidesToShow: 1,
